e8a5a6c633d891a6c2a2ec261c35eda406e020b4
rkdeofuf <asa47972000@naver.com>
2024-08-06 19:02:00 +0800

    add profile, modal

---
+++ mongoclass/gallery/js/pictureList.js
@@ -0,0 +1,26 @@
+const PICTURE_SRC_LIST = [
+    {
+        path: './image/sample.jpg',
+        title: 'title contents',
+        subTitle: 'Laboris ut reprehenderit amet dolore.',
+    },
+    {
+        path: './image/sample.jpg',
+        title: 'title contents',
+        subTitle: 'Non ad laboris duis veniam sunt sit.',
+    },
+    {
+        path: './image/sample.jpg',
+        title: 'title contents',
+        subTitle:
+            'Veniam qui labore proident proident pariatur veniam laboris aute elit mollit aliquip sit.',
+    },
+    {
+        path: './image/sample.jpg',
+        title: 'title contents',
+        subTitle:
+            'Ex exercitation incididunt labore non tempor commodo sunt nisi consectetur nulla.',
+    },
+]
+
+export default PICTURE_SRC_LIST
